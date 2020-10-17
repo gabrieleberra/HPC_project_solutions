889a58c577cc06819d32cdbb32770a5ba6491bb9
Gabriele Berra <gabriele.berra@usi.ch>
2020-10-17 19:12:07 +0200

    Modified sequantial mandelbrot and added parallel folder

--- project2/src/mandel/consts.h
+++ project2/src/mandel/consts.h
@@ -5,10 +5,10 @@
 #define MAX_ITERS     35207
 
 // image size
-#define IMAGE_WIDTH   4096
-#define IMAGE_HEIGHT  4096
+#define IMAGE_WIDTH  4096
+#define IMAGE_HEIGHT   4096
 
-// the extent of the parameter plane ( MIN_X + iMIN_Y <= c < MAX_X + iMAX_Y ) 
+// the extent of the parameter plane ( MIN_X + iMIN_Y <= c < MAX_X + iMAX_Y )
 #define MIN_X   -2.1
 #define MAX_X    0.7
 #define MIN_Y   -1.4
